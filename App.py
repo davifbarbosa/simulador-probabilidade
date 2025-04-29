5a443925b58f4f320570cfb7a1c8936c169f3b70
davifbarbosa <79879015+davifbarbosa@users.noreply.github.com>
2025-04-29 16:44:35 -0300

    Update App.py

--- App.py
+++ App.py
@@ -5,8 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import gaussian_kde
 
-st.title("🔮 Simulador de Previsão de Vendas")
-st.markdown("Cole abaixo seus dados de vendas separadas por vírgula. Exemplo: `32000, 38000, 85000, 18000, ...`")
+st.title("🔮 Sales Planning Simulator")
+st.markdown("Paste below your sales data like this example: `32000, 38000, 85000, 18000, ...`")
 
 # Dados de vendas como exemplo preenchido (os seus 23 meses)
 exemplo_dados = "22577,72072,31977,38108,85599,18716,28645,19905,13607,21550,17133,18742,34097,24848,21072,36385,45547,16413,21205,30540,19279,21525,15210,32372,26480,39244,18120,20648,41759,17391,8200,11101,47600,43749,24087,27440,21466"
@@ -24,17 +24,17 @@ if input_str:
         valor_mais_provavel = x[np.argmax(y)]
 
         # Mostra resultado
-        st.subheader(f"📌 Valor mais provável: **{valor_mais_provavel:,.0f}** unidades")
+        st.subheader(f"📌 Most Likely value: **{valor_mais_provavel:,.0f}** unidades")
 
         # Mostra gráfico
         fig, ax = plt.subplots()
-        ax.plot(x, y, color="orange", label="Densidade estimada")
-        ax.axvline(valor_mais_provavel, color="red", linestyle="--", label=f"Mais provável: {valor_mais_provavel:,.0f}")
-        ax.set_xlabel("Vendas")
-        ax.set_ylabel("Densidade")
+        ax.plot(x, y, color="orange", label="Estimated Density")
+        ax.axvline(valor_mais_provavel, color="red", linestyle="--", label=f"Most Likely: {valor_mais_provavel:,.0f}")
+        ax.set_xlabel("Sales")
+        ax.set_ylabel("Density")
         ax.legend()
         ax.grid(True)
         st.pyplot(fig)
 
     except Exception as e:
-        st.error(f"Erro ao processar os dados: {e}")
+        st.error(f"Error Processing data: {e}")
